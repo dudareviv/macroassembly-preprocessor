ad59409fa55caf4d74a990e48bef2a6796720b8e
Dudarev Ilia <dudareviv@gmail.com>
2015-12-25 13:02:48 +0400

    Первая инициализация

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
+            this.fileNameField = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(12, 38);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(111, 23);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Выбрать файл";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // runButton
+            // 
+            this.runButton.Enabled = false;
+            this.runButton.Location = new System.Drawing.Point(294, 38);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 1;
+            this.runButton.Text = "Запустить";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // fileNameField
+            // 
+            this.fileNameField.Enabled = false;
+            this.fileNameField.Location = new System.Drawing.Point(12, 12);
+            this.fileNameField.Name = "fileNameField";
+            this.fileNameField.Size = new System.Drawing.Size(357, 20);
+            this.fileNameField.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 72);
+            this.Controls.Add(this.fileNameField);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.openFileButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox fileNameField;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Файлы макроассемблера|*.masm|Все файлы (*.*)|*.*";
+            fileDialog.Title = "Выберите файл макроассемблера";
+
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+                //fileNameField.Text = sr.ReadToEnd();
+                //sr.Close();
+                fileNameField.Text = fileDialog.FileName;
+                this.runButton.Enabled = true;
+            }
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamReader sr = new System.IO.StreamReader(fileNameField.Text);
+            MessageBox.Show(sr.ReadToEnd());
+            sr.Close();
+        }
+    }
+}
